b7e132f6679003108d7d1d9abad825f06fdfb8a0
HICHAM-RK <rkibaa.hicham@gmail.com>
2025-01-14 05:16:29 +0100

    load

---
+++ load.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    setTimeout(() => {
+      // Hide the loader
+      document.getElementById("loader").style.display = "none";
+  
+      // Show the main content
+      document.getElementById("main-content").style.display = "block";
+    }, 2000); // 
+  };
+  
